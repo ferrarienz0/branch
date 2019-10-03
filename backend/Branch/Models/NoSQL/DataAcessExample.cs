2aa836311daf2f399fc3d78e07a8736536b15a9d
Enzo Ferrari <ferrarienzobf@gmail.com>
2019-10-03 11:41:07 -0300

    Adicionei a base do MongoDB e exemplos de Acess e Models.
    @mapaf Terminar as models

---
+++ backend/Branch/Models/NoSQL/DataAcessExample.cs
@@ -0,0 +1,20 @@
+﻿using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Branch.Models.NoSQL
+{
+    public class DataAcessExample
+    {
+        MongoClient Client;
+        IMongoDatabase DataBase;
+
+        public DataAcessExample()
+        {
+            Client = new MongoClient("CONNECTION_STRING");
+            DataBase = Client.GetDatabase("DATABASE_NAME");
+        }
+    }
+}
