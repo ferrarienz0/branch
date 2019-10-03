8f0e9408d96c387ea0600b11dcf72662c3c25874
oLucasRez <Lucas_rez@outlook.com>
2019-10-03 18:50:51 -0300

    update

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,7 +1,28 @@
 import React, { Component } from 'react';
+import { IoIosPerson } from 'react-icons/io';
+import './Home.css';
+import icone from '../assets/icone.svg';
 
 export default class Home extends Component {
     render() {
-        return <h1>Hello</h1>;
+        return (
+            <div className="home-container">
+                <link
+                    href="https://fonts.googleapis.com/icon?family=Material+Icons"
+                    rel="stylesheet"
+                />
+                <div className="head">
+                    <img src={icone} alt="Branch" />
+                </div>
+                <div className="body">
+                    <div className="perfil">
+                        <div className="photo">
+                            <IoIosPerson />
+                        </div>
+                    </div>
+                    <div className="feed"></div>
+                </div>
+            </div>
+        );
     }
 }
