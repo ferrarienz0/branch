88946130388ed96d6a75897ba66b838add70f432
Lucas Rezende <arrobagemeioponto@gmail.com>
2019-10-08 16:19:31 -0300

    mudanças em Home

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -34,7 +34,7 @@ export default class Home extends Component {
                     </div>
                     <div className="feed">
                         <div className="myPost">
-                            <input type="text" />
+                            <textarea type="text" />
                             <div className="optionsPost">
                                 <button className="send">
                                     <FaRegPaperPlane className="sendIcon" />
