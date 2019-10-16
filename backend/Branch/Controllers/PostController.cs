526a3d0dd21d9dd49e9773d0aea0498dc381bdfe
Enzo Ferrari <ferrarienzobf@gmail.com>
2019-10-16 12:43:13 -0300

    Ativei o cors nos controllers =)

--- backend/Branch/Controllers/PostController.cs
+++ backend/Branch/Controllers/PostController.cs
@@ -6,9 +6,11 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using System.Web.Http.Cors;
 
 namespace Branch.Controllers
 {
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
     public class PostController : ApiController
     {
         [HttpPost]
--- backend/Branch/Controllers/UserController.cs
+++ backend/Branch/Controllers/UserController.cs
@@ -5,9 +5,11 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using System.Web.Http.Cors;
 
 namespace Branch.Controllers
 {
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
     public class UserController : ApiController
     {
         [HttpPost]
